2ae0b982128673cd7d1f5055abc66dfe6125744b
Umang-Nayak <146411216+Umang-Nayak@users.noreply.github.com>
2023-11-28 17:36:12 +0530

    Update settings.py

--- Modern/settings.py
+++ Modern/settings.py
@@ -138,4 +138,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kingumang639@gmail.com'
-EMAIL_HOST_PASSWORD = 'ottbreqkdablltqw'
+EMAIL_HOST_PASSWORD = ''
